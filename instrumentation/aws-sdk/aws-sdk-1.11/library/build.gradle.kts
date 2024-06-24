c28be72d9b67c0835ce1d3800acb7b59c0b94944
Zhonghao Zhao <zzhlogin@amazon.com>
2024-06-24 13:10:50 -0700

    Remove bedrock unit tests.

--- instrumentation/aws-sdk/aws-sdk-1.11/library/build.gradle.kts
+++ instrumentation/aws-sdk/aws-sdk-1.11/library/build.gradle.kts
@@ -4,7 +4,11 @@ plugins {
 
 dependencies {
   implementation("io.opentelemetry.contrib:opentelemetry-aws-xray-propagator")
-  implementation("org.json:json:20210307")
+  implementation("org.json:json") {
+    version {
+      strictly("[20210307,)")
+    }
+  }
 
   library("com.amazonaws:aws-java-sdk-core:1.11.0")
   library("com.amazonaws:aws-java-sdk-sqs:1.11.106")
@@ -18,10 +22,10 @@ dependencies {
   testLibrary("com.amazonaws:aws-java-sdk-kinesis:1.11.106")
   testLibrary("com.amazonaws:aws-java-sdk-dynamodb:1.11.106")
   testLibrary("com.amazonaws:aws-java-sdk-sns:1.11.106")
-  testLibrary("com.amazonaws:aws-java-sdk-bedrock:1.12.744")
-  testLibrary("com.amazonaws:aws-java-sdk-bedrockagent:1.12.744")
-  testLibrary("com.amazonaws:aws-java-sdk-bedrockagentruntime:1.12.744")
-  testLibrary("com.amazonaws:aws-java-sdk-bedrockruntime:1.12.744")
+//  testLibrary("com.amazonaws:aws-java-sdk-bedrock:1.12.744")
+//  testLibrary("com.amazonaws:aws-java-sdk-bedrockagent:1.12.744")
+//  testLibrary("com.amazonaws:aws-java-sdk-bedrockagentruntime:1.12.744")
+//  testLibrary("com.amazonaws:aws-java-sdk-bedrockruntime:1.12.744")
 
   // last version that does not use json protocol
   latestDepTestLibrary("com.amazonaws:aws-java-sdk-sqs:1.12.583")
